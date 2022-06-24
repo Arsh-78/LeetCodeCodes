baec1ea1ff4683c316e336f0d394fea8b94ba0ae
Arsh-78 <76211430+Arsh-78@users.noreply.github.com>
2022-06-24 19:42:22 +0530

    Time: 33 ms (10.83%), Space: 40 MB (81.21%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -9,20 +9,20 @@ public class Solution extends VersionControl {
         while(start <=end)
         {
             mid = start + (end-start)/2;
-            if(isBadVersion(mid))
+            if(!isBadVersion(mid))
             {
-                if(!isBadVersion(mid-1))
-                {
-                    return mid;
-                }
-                else
-                {
-                    end = mid-1;
-                }
+                start = mid +1;
+            }
+            else if(!isBadVersion(mid-1))
+            {
+                
+                
+                    return  mid;
+                
             }
             else 
             {
-                start = mid+1;
+                end = mid-1;
             }
                      
         }
