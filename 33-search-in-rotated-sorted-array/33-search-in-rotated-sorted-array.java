2580f723d0e0b17be5e4b0f25105d5f031b5cae3
Arsh-78 <76211430+Arsh-78@users.noreply.github.com>
2022-06-25 00:04:53 +0530

    Time: 1 ms (70.62%), Space: 42.3 MB (70.92%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,42 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start=0 ,end=nums.length-1,mid=0,pivot=0;
+        int start=0 ,end=nums.length-1,mid=0;
+        int pivot = 0;
+        
         int last_element = nums[end];
         int ans;
      
-        if(nums[start]<nums[end] || nums.length==1)
+       if(nums[start]<nums[end] || nums.length==1)
         {
             return bS(nums,start,end,target);
         }
-        while(start<end)
+        
+        while(start<=end)
         {
-            if(nums[0]>nums[1])
-            {
-                pivot = 0;
-                break;
-            }
             mid=start +(end-start)/2;
-            if((nums[mid]>nums[mid+1] && nums[mid] >nums[mid-1]))
-            {   
-                pivot = mid;
-                break;
-            }
-            else if((nums[mid]<nums[mid+1] && nums[mid]<nums[mid-1]))
+            if(mid==0)
             {
-                pivot = mid-1;
+                start++;
+            }
+            else if(nums[mid-1]>nums[mid])
+            {
+                pivot=mid-1;
                 break;
             }
-            else if((nums[mid] <nums[mid+1] && nums[mid] >nums[mid-1]) 
-                   && nums[mid]>last_element)
+            else if(nums[end]<nums[mid])
             {
                 start = mid+1;
             }
             else
             {
-                end = mid-1;
+                end=mid-1;
             }
+          
         }
-        System.out.println(pivot);
+     
         if(target <=last_element)
         {
             ans =bS(nums,pivot+1,nums.length-1,target);
@@ -48,6 +45,23 @@ class Solution {
         return ans;
         
     }
+      /*public int findPivot(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(mid == 0){
+                start++;
+            } else if(nums[mid - 1] > nums[mid]){
+                return mid;
+            } else if(nums[end] < nums[mid]){
+                start = mid + 1;
+            } else{
+                end = mid - 1;
+            }
+        }
+        return 0;
+    }*/
     
     int bS(int[] nums,int start,int end,int target)
     {
