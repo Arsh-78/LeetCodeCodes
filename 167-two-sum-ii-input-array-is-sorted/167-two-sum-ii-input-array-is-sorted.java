7ae682d89a247be88f25e948ba786e14eb109f80
Arsh-78 <76211430+Arsh-78@users.noreply.github.com>
2022-06-24 21:46:35 +0530

    Time: 116 ms (6.11%), Space: 55.3 MB (5.18%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,28 +1,50 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int end = numbers.length -1;
         int[] ans = new int[2];
-        int start = 0;
-        while(start<end)
+        
+        for(int i = 0;i<numbers.length;i++)
         {
-            if(numbers[start] + numbers[end]==target)
+            int nt = target-numbers[i];
+            System.out.println(nt);
+            int x = bS(numbers,nt,i);
+            
+            if(x == -1001)
             {
-                ans[0]=start+1;
-                ans[1]=end+1; 
-                return ans;
+                continue;
             }
             else
             {
-                if(numbers[start] + numbers[end]<target)
-                {
-                    start++;
-                }
-                else
-                {
-                    end--;
-                }
+                System.out.println(i);
+                ans[0]=i+1;
+                ans[1]=x+1;
+                break;
             }
         }
         return ans;
 }
+    
+    int bS(int[] numbers,int target,int index)
+    {
+        int end = numbers.length-1;
+        int start = 0;
+        while(start<=end)
+        {
+            int mid = start+(end-start)/2;
+            if(numbers[mid] == target && mid != index)
+            {
+                
+                return mid;
+            }
+            else if(numbers[mid]>target)
+            {
+            end = mid-1;
+            }
+            else
+            {
+                start = mid +1;
+            }
+                
+        }
+        return -1001;
+    }
 }
