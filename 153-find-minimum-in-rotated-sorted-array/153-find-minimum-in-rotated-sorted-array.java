15dad8faefd445ac4e51e3071c04c54150cd591b
Arsh-78 <76211430+Arsh-78@users.noreply.github.com>
2022-06-26 20:38:08 +0530

    Time: 3 ms (39.94%), Space: 43.7 MB (6.33%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -21,7 +21,7 @@ class Solution {
     {
         int start =0;
         int end = nums.length-1;
-        int mid;
+        int mid=0;
         while(start <= end)
         {
             mid=start + (end-start)/2;
@@ -42,6 +42,6 @@ class Solution {
                 start = mid+1;
             }
         }
-        return -1;
+        return mid;
     }
 }
