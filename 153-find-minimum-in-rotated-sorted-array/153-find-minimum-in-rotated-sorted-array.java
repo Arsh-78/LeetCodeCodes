f4789a1af2e05920ce063080ea6dacc6f1d9d694
Arsh-78 <76211430+Arsh-78@users.noreply.github.com>
2022-06-26 20:48:03 +0530

    Time: 1 ms (39.94%), Space: 42.2 MB (67.25%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,47 +1,19 @@
 class Solution {
     public int findMin(int[] nums) {
+        int low = 0;
+int high = nums.length-1;
+while(low<high){
+int mid=(low+high)/2;
+if(nums[mid]>nums[high])
+low=mid+1;
+else if(nums[mid]<nums[high])
+high=mid;
+else if(nums[mid]>nums[low])
+high=mid-1;
+else if(nums[mid]<nums[low])
+low=mid;
+}
+return nums[low];
         
-        if(nums[0]<nums[nums.length-1])
-        {
-            return nums[0];
-        }
-       int pivot =pivotNum(nums);
-        System.out.println(pivot);
-        if(pivot==0 && nums.length!=1)
-        {
-            return nums[1];
-        }
-        
-        return nums[pivot];
-        
-        
-    }
-    
-    int pivotNum(int[] nums)
-    {
-        int start =0;
-        int end = nums.length-1;
-        int mid=0;
-        while(start <= end)
-        {
-            mid=start + (end-start)/2;
-            if(mid == nums.length-1 || mid==0)
-            {
-                return mid;
-            }
-            if(nums[mid]<nums[mid-1])
-            {
-                return mid;
-            }
-            if(nums[mid]<nums[mid+1] && nums[mid]<nums[nums.length-1])
-            {
-                end=mid-1;
-            }
-            else
-            {
-                start = mid+1;
-            }
-        }
-        return mid;
     }
 }
