005cec8b70eead8362ebc9006a02ff04df6a2635
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-03-07 10:28:57 +0530

    Time: 424 ms (7.62%), Space: 51.5 MB (97.60%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -1,6 +1,6 @@
 class Solution {
     public long minimumTime(int[] time, int totalTrips) {
-        long low=Long.MAX_VALUE;
+         long low=Long.MAX_VALUE;
         long high=0;
         long min=Long.MAX_VALUE;
         for(int it:time){
@@ -8,23 +8,33 @@ class Solution {
             min=Math.min(min,it);
         }
         high=totalTrips*min;
-        while(low<high){
-            long mid=low+(high-low)/2;
-            if(blackbox(mid,totalTrips,time)){
-                high=mid;
+        while(low<high)
+        {
+            long mid = low +(high-low)/2;
+            System.out.println(mid + " : "+canTbm(mid,time,totalTrips));
+            if(canTbm(mid,time,totalTrips))
+            {
+                high = mid;
+                
             }
             else
-                low=mid+1;
+            {
+                low = mid+1;
+            }
         }
         return low;
     }
-    public boolean blackbox(long isvalidtime,int totalTrips,int[] time){
-        long trips=0;
-        for(int it:time){
-            trips+=isvalidtime/it;
+    
+    public boolean canTbm(long t , int[] time,int totaltrips)
+    {
+        
+        long tTrips=0;
+        for(int i=0;i<time.length;i++)
+        {
+            tTrips += t/time[i];
         }
-        if(trips>=totalTrips)
-            return true;
-        return false;
+        
+        return tTrips>=totaltrips ;
+        
     }
 }
