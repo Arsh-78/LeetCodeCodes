5842e305a6587035318461e4af800c0f5d2534a7
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-06-16 12:29:19 +0530

    Time: 2 ms (80.85%), Space: 56.8 MB (36.49%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+
+     
+     int f = 0;
+     int t=0;
+
+     for(int i=0;i<bills.length;i++)
+     {
+         if(bills[i]==5)
+         {
+             f++;
+         }
+         else if(bills[i]==10)
+         {
+             t++;
+             f--;
+         }
+         else
+         {
+             if(t>0)
+             {
+                 t--;
+                 f--;
+             }
+             else
+             {
+                 f=f-3;
+             }
+         }
+
+      if(f<0 || t<0)
+      {
+         return false;
+      }
+     }
+     
+     return true;
+    }
+}
