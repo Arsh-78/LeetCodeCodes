482d250351de34f6081214cbefb53e93a851facd
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-06-26 14:53:58 +0530

    Time: 0 ms (100%), Space: 44.3 MB (38%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,5 +1,6 @@
 class Solution {
      public boolean isBipartite(int[][] graph) {
+         //Bipartite mein do color meon divide ho jaana chahiye graph aur components ho skte hai to baar baar check krk same color se dfs call kr rhe
         int[] colors = new int[graph.length];
         Arrays.fill(colors, -1);
         for(int u = 0; u < graph.length; u++){
@@ -13,10 +14,15 @@ class Solution {
     }
     
     public boolean dfs(int u, int[][]graph, int[]colors, int col){
-        if (colors[u] == 1 - col){
-            return false; 
-        } 
-        if (colors[u] == col) {
+        // if (colors[u] == 1- col){ //basically wo condition jisme required color se alg hai
+        //     return false; 
+        // } 
+
+        if(colors[u] !=-1 && colors[u] != col)
+        {
+            return false;
+        }
+        if (colors[u] == col) { //the current vertices is already colored with required color
             return true; 
         }
         colors[u] = col;
