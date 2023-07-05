dc4124b98dc8984fe5faf101465dc8719f186f65
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-07-06 01:31:37 +0530

    Time: 52 ms (65.49%), Space: 55.6 MB (23.88%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,12 +6,26 @@ class Solution {
         {
             total+=nums[i];
         }
-        int[][] dp = new int[nums.length][total+1];
+        if(total%2 !=0)
+        {
+            return false;
+        }
+        boolean[][] dp = new boolean[nums.length][total*2];
         for(int i=0;i<nums.length;i++)
         {
-            Arrays.fill(dp[i],-1);
+            dp[i][total/2]=true;
         }
-        return canTw(nums.length-1,0,nums,total,dp);
+        for(int i=1;i<nums.length;i++)
+        {
+            for(int target = total/2;target>=0;target--)
+            {
+                
+                boolean take = dp[i-1][target+nums[i]];
+                boolean ntake = dp[i-1][target];
+                dp[i][target] = take | ntake;
+            }
+        }
+        return dp[nums.length-1][0];
     }
 
     public boolean canTw(int i,int curT,int[] nums ,int total,int[][] dp)
@@ -28,7 +42,6 @@ class Solution {
         {
             return dp[i][curT]==1;
         }
-//System.out.println("index: "+i+" current subset sum: "+curT);
         boolean take = canTw(i-1,curT+nums[i],nums,total,dp);
         boolean ntake = canTw(i-1,curT,nums,total,dp);
 
@@ -38,3 +51,48 @@ class Solution {
         
     }
 }
+
+
+
+//Memoization solution
+
+// class Solution {
+//     public boolean canPartition(int[] nums) {
+
+//         int total =0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             total+=nums[i];
+//         }
+//         int[][] dp = new int[nums.length][total+1];
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             Arrays.fill(dp[i],-1);
+//         }
+//         return canTw(nums.length-1,0,nums,total,dp);
+//     }
+
+//     public boolean canTw(int i,int curT,int[] nums ,int total,int[][] dp)
+//     {
+//         if(curT*2 == total)
+//         {
+//             return true;
+//         }
+//         if(i<0)
+//         {
+//             return false;
+//         }
+//         if(dp[i][curT]!=-1)
+//         {
+//             return dp[i][curT]==1;
+//         }
+// //System.out.println("index: "+i+" current subset sum: "+curT);
+//         boolean take = canTw(i-1,curT+nums[i],nums,total,dp);
+//         boolean ntake = canTw(i-1,curT,nums,total,dp);
+
+//         dp[i][curT] = take | ntake== true ? 1 : 0;
+
+//         return take | ntake;
+        
+//     }
+// }
