5449ee2d6fa15b44480d6108358c3e8b2f66b5f8
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-07-08 23:27:51 +0530

    Time: 24 ms (41.97%), Space: 44.4 MB (11.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -4,7 +4,7 @@ class Solution {
         int[][] dp =new int[n][amount+1];
         for(int i=0;i<n;i++)
         {
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],0);
         }
         if(coins.length==1)
         {
@@ -14,8 +14,31 @@ class Solution {
             }
             return -1;
         }
-        int ans=coinCM(n-1,coins,amount,dp);
-        return  ans == 1e9 ? -1 :ans;
+
+        for(int i=0;i<=amount;i++)
+        {
+            if(i%coins[0]==0)
+                dp[0][i]=i/coins[0];
+            else
+                dp[0][i]=(int)1e9;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int t = 0;t<=amount;t++)
+            {
+                    int take = Integer.MAX_VALUE;
+                    if(coins[i] <= t)
+                    {
+                        take = 1+dp[i][t-coins[i]];
+                    }
+                    int ntake = dp[i-1][t];
+                    dp[i][t]=Math.min(ntake,take);
+            }
+        }
+        //int ans=coinCM(n-1,coins,amount,dp);
+        //return  ans == 1e9 ? -1 :ans;
+
+        return dp[n-1][amount]==1e9 ? -1 : dp[n-1][amount];
     }
 
     int coinCM(int i ,int[] coins,int amt ,int[][] dp)
