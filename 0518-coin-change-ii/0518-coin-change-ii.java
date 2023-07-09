d59544977f3db2b9e496ccbb31e4bf77d57a2759
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-07-09 14:11:34 +0530

    Time: 6 ms (65.9%), Space: 46.3 MB (18.73%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n= coins.length;
+        int[][] dp =new int[n][amount+1];
+        for(int i=0;i<n;i++)
+        {
+            Arrays.fill(dp[i],-1);
+        }
+        return changeM(n-1,amount,coins,dp);
+    }
+
+    public int changeM(int i,int amt,int[] coins,int[][] dp) 
+    {
+        if(i==0)
+        {
+            if(amt%coins[0]==0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        if(dp[i][amt] !=-1)
+        {
+            return dp[i][amt];
+        }
+        int ntake= changeM(i-1,amt,coins,dp);
+        int take =0;
+        if(amt>=coins[i])
+        {
+            take= changeM(i,amt-coins[i],coins,dp);
+        }
+
+        return dp[i][amt]=take+ntake;
+    }
+}
