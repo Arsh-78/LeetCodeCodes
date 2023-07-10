5b5fee3834a38e8844943440fd200801063f6cf4
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-07-10 14:11:43 +0530

    Time: 46 ms (29.54%), Space: 44.7 MB (71.58%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -8,9 +8,24 @@ class Solution {
         String s2 = sb.reverse().toString();
         for(int i=0;i<=n;i++)
         {
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],0);
+
         }
-        return n-minInM(n,n,s,s2,dp);
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s.charAt(i-1) == s2.charAt(j-1))
+                {
+                     dp[i][j]= 1+ dp[i-1][j-1]; 
+                }
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+ 
+            }
+        }
+        return n-dp[n][n];
         
     }
 
