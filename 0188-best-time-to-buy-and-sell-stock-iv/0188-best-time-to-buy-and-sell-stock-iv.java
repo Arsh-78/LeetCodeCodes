356e69be7ceb64d4f9fef701d2f5e1c2bece0cca
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-07-18 23:24:17 +0530

    Time: 3 ms (78.00%), Space: 44 MB (24.19%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,13 +1,41 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        int[][][] dp = new int[prices.length][2][k+1];
-        for(int i=0;i<prices.length;i++)
+        int n=prices.length;
+        int[][][] dp = new int[prices.length+1][2][k+1];
+        for(int i=0;i<=prices.length;i++)
         {
-            Arrays.fill(dp[i][0],-1);
-            Arrays.fill(dp[i][1],-1);
+            Arrays.fill(dp[i][0],0);
+            Arrays.fill(dp[i][1],0);
         }
-
-        return maxm(0,prices,k,0,dp);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                for(int cap=1;cap<=k;cap++)
+                {
+                    
+                    int o=0; //two random variables for checking states
+                    int t=0;
+                    if(j == 1) // permitted to sell but not buy
+                    {
+                        o = dp[i+1][1][cap];
+                        t= prices[i] + dp[i+1][0][cap-1];
+                         dp[i][j][cap]=Math.max(o,t);
+                    }
+                    else
+                    {
+                        o=dp[i+1][1][cap]-prices[i];
+                        t=dp[i+1][0][cap];
+                        dp[i][j][cap]=Math.max(o,t);
+                    }
+                }
+                
+            }
+        }
+        return dp[0][0][k];
+        
+        //return maxm(0,prices,k,0,dp);
     }
 
     public int maxm(int i,int[] prices,int cap , int trans,int[][][] dp)
