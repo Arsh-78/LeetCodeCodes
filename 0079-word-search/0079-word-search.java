2485f2ba057e752e78cd690a9fdd497da1d50c61
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-08-18 10:12:35 +0530

    Time: 347 ms (14.11%), Space: 44.1 MB (14.62%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,67 +1,55 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-
-     boolean[][] vis = new boolean[board.length][board[0].length]; 
-
-     for(int i = 0 ; i<board.length;i++)
-     {
-         for(int j=0;j<board[0].length;j++)
-         {
-             if(board[i][j]==word.charAt(0))
-             {
-                 if(helper(board,vis,word,0,i,j))
-                    return true;
-             }
-         }
-     }  
-     return false;
+     
+        boolean[][] vis = new boolean[board.length][board[0].length];
+        
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++)
+            {
+                if(board[i][j]==word.charAt(0))
+                {
+                    if(helper(0,i,j,board,word,vis))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
     }
-
-    public boolean helper(char[][] board,boolean[][] vis,String word,int i, int r,int c)
+    
+    public boolean helper(int j,int r,int c,char[][] board, String word,boolean[][] vis)
     {
-        if(i>word.length()-1)
+        if(j==word.length()-1)
         {
             return true;
         }
-        if(r>=board.length || c>=board[0].length||r<0||c<0)
+        vis[r][c]=true;
+        
+        int[][] dirs = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
+        
+        boolean a = false;
+        for(int i=0;i<dirs.length;i++)
         {
-            return false;
+            int nr = r+dirs[i][0];
+            int nc = c + dirs[i][1];
+            
+            if(isValid(j+1,nr,nc,board,vis,word))
+                a= a | helper(j+1,nr,nc,board,word,vis);
         }
-        if(vis[r][c]==true)
-        {
-            return false;
-        }
-        if(board[r][c]!=word.charAt(i))
-        {
-            return false;
-        }
-        else
-        {
-            vis[r][c]=true;
-            boolean down=false,up=false,l=false,rg=false;
-            if(r<board.length)
-            {
-                down=helper(board,vis,word,i+1,r+1,c);
-            }
-            if(r>0)
-            {
-                up=helper(board,vis,word,i+1,r-1,c);
-                
-            }
-             if(c<board[0].length)
-            {
-                rg=helper(board,vis,word,i+1,r,c+1);
-            }
-            if(c>0)
-            {
-                l=helper(board,vis,word,i+1,r,c-1);
-                
-            }
-
-            boolean ans = up||down||l||rg;
-            vis[r][c]=false;
-            return ans;
-        }
-
+        vis[r][c]=false;
+        return a;
+        
+        
+    }
+    
+    public boolean isValid(int i,int r,int c,char[][] board,boolean[][] vis,String word)
+    {
+        if(r>=board.length || c>=board[0].length ||r<0||c<0 || vis[r][c]==true || word.charAt(i) != board[r][c])
+             return false;
+        
+        return true;
     }
 }
