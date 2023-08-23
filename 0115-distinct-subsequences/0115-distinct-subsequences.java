b45e6bfb0877c594a815d9cd68e35e2a8b96b7a8
Arsh Ansari <76211430+Arsh-78@users.noreply.github.com>
2023-08-23 10:48:36 +0530

    Time: 18 ms (54.82%), Space: 47.3 MB (16.73%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -4,11 +4,25 @@ class Solution {
         int[][] dp = new int[s.length()+1][t.length()+1];
         for(int i=0;i<=s.length();i++)
         {
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],0);
+            dp[i][0]=1;
         }
-        int a =ds(s.length(),t.length(),s,t,dp);
-        //System.out.println(Arrays.toString(dp));
-        return a;
+        
+        for(int i=1;i<=s.length();i++)
+        {
+            for(int j=1;j<=t.length();j++)
+            {
+                if(s.charAt(i-1)==t.charAt(j-1))
+                {
+                     dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+                }
+                else           
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        
+        return dp[s.length()][t.length()];
+        
     }
     public int ds(int i,int j,String s,String t,int[][] dp)
     {
